ca7dc84312bdf58eb263d46f51c5b8949512f31c
r00tVen0m <50659263+r00tVen0m@users.noreply.github.com>
2026-07-29 13:03:08 +0100

    AAAA

--- grant-restore-rights.ps1
+++ grant-restore-rights.ps1
@@ -1,42 +1,18 @@
 $DomainDN = (Get-ADDomain).DistinguishedName
 $Acl = Get-Acl "AD:\$DomainDN"
 
-$Identity = New-Object System.Security.Principal.SecurityIdentifier((Get-ADUser svc_recovery).SID)
-$ReanimateGuid = New-Object Guid "45ec5156-db7e-47bb-b53f-dbeb2d03c40f"
+$RulesToRemove = $Acl.Access | Where-Object { $_.IdentityReference -like "*svc_recovery*" }
 
-$Rule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule(
-    $Identity,
-    [System.DirectoryServices.ActiveDirectoryRights]::ExtendedRight,
-    [System.Security.AccessControl.AccessControlType]::Allow,
-    $ReanimateGuid
-)
-
-$Acl.AddAccessRule($Rule)
-Set-Acl "AD:\$DomainDN" $Acl
-
-
-$Error.Clear()
-try {
-    $DomainDN = (Get-ADDomain).DistinguishedName
-    $Acl = Get-Acl "AD:\$DomainDN"
-    $Identity = New-Object System.Security.Principal.SecurityIdentifier((Get-ADUser svc_recovery).SID)
-    $ReanimateGuid = New-Object Guid "45ec5156-db7e-47bb-b53f-dbeb2d03c40f"
-    $Rule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule(
-        $Identity,
-        [System.DirectoryServices.ActiveDirectoryRights]::ExtendedRight,
-        [System.Security.AccessControl.AccessControlType]::Allow,
-        $ReanimateGuid
-    )
-    $Acl.AddAccessRule($Rule)
-    Set-Acl "AD:\$DomainDN" $Acl -ErrorAction Stop
-    Write-Host "تم التنفيذ بدون خطأ" -ForegroundColor Green
-} catch {
-    Write-Host "خطأ: $_" -ForegroundColor Red
+foreach ($rule in $RulesToRemove) {
+    $Acl.RemoveAccessRule($rule) | Out-Null
 }
 
+Set-Acl "AD:\$DomainDN" $Acl
 
-$Acl2 = Get-Acl "AD:\$DomainDN"
-$Acl2.Access | Where-Object { $_.IdentityReference -like "*svc_recovery*" }
+dsacls "CN=Deleted Objects,DC=astera,DC=cg" /R "ASTERA\svc_recovery"
+
+dsacls "OU=Users,OU=CORP,DC=astera,DC=cg" /R "ASTERA\svc_recovery"
 
 
+dsacls "CN=Deleted Objects,DC=astera,DC=cg" /takeownership
 dsacls "CN=Deleted Objects,DC=astera,DC=cg" /G "ASTERA\svc_recovery:LCRPWPCCDCRCLO" /I:T
