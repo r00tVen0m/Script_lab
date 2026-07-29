977126d70db58881f1db7b46ee19a9a3ee69fdf1
r00tVen0m <50659263+r00tVen0m@users.noreply.github.com>
2026-07-29 15:44:09 +0100

    qqq

--- grant-restore-rights.ps1
+++ grant-restore-rights.ps1
@@ -3,7 +3,7 @@ dsacls "CN=Deleted Objects,DC=astera,DC=cg" /takeownership
 
 dsacls "CN=Deleted Objects,DC=astera,DC=cg" /R "ASTERA\svc_recovery"
 dsacls "CN=Deleted Objects,DC=astera,DC=cg" /G "ASTERA\svc_recovery:SDRPWOCCDCLCWSWPRPRC"
-
+dsacls "OU=DeletedUsers,OU=CORP,DC=astera,DC=cg" /R "ASTERA\Recovery Operators"
 
 ----------------------
 
@@ -13,3 +13,9 @@ dsacls "CN=Deleted Objects,DC=astera,DC=cg" /G "ASTERA\Recovery Operators:LCRP"
 dsacls "OU=DeletedUsers,OU=CORP,DC=astera,DC=cg" /I:T /G "ASTERA\Recovery Operators:WPCC"
 
 Get-ADObject -Filter 'isDeleted -eq $true' -IncludeDeletedObjects
+
+
+dsacls "OU=DeletedUsers,OU=CORP,DC=astera,DC=cg" /G "ASTERA\Recovery Operators:CCDC;user"
+dsacls "OU=DeletedUsers,OU=CORP,DC=astera,DC=cg" /G "ASTERA\Recovery Operators:WP;user"
+dsacls "OU=DeletedUsers,OU=CORP,DC=astera,DC=cg" /G "ASTERA\Recovery Operators:RP;user"
+dsacls "OU=DeletedUsers,OU=CORP,DC=astera,DC=cg" /G "ASTERA\Recovery Operators:RC"
