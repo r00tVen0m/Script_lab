3caa86dbf840a841439e2ce03837e52211b3c5b8
r00tVen0m <50659263+r00tVen0m@users.noreply.github.com>
2026-07-29 13:08:39 +0100

    SSS

--- grant-restore-rights.ps1
+++ grant-restore-rights.ps1
@@ -16,3 +16,16 @@ dsacls "OU=Users,OU=CORP,DC=astera,DC=cg" /R "ASTERA\svc_recovery"
 
 dsacls "CN=Deleted Objects,DC=astera,DC=cg" /takeownership
 dsacls "CN=Deleted Objects,DC=astera,DC=cg" /G "ASTERA\svc_recovery:LCRPWPCCDCRCLO" /I:T
+
+$DomainDN = (Get-ADDomain).DistinguishedName
+$Acl = Get-Acl "AD:\$DomainDN"
+$Identity = New-Object System.Security.Principal.SecurityIdentifier((Get-ADUser svc_recovery).SID)
+$ReanimateGuid = New-Object Guid "45ec5156-db7e-47bb-b53f-dbeb2d03c40f"
+$Rule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule(
+    $Identity,
+    [System.DirectoryServices.ActiveDirectoryRights]::ExtendedRight,
+    [System.Security.AccessControl.AccessControlType]::Allow,
+    $ReanimateGuid
+)
+$Acl.AddAccessRule($Rule)
+Set-Acl "AD:\$DomainDN" $Acl
