e4ce8500ee5c121dffcb8c2f619c32677b44fbaa
r00tVen0m <50659263+r00tVen0m@users.noreply.github.com>
2026-07-29 15:16:31 +0100

    AAA

--- grant-restore-rights.ps1
+++ grant-restore-rights.ps1
@@ -7,14 +7,9 @@ dsacls "CN=Deleted Objects,DC=astera,DC=cg" /G "ASTERA\svc_recovery:SDRPWOCCDCLC
 
 ----------------------
 
-$DomainDN = (Get-ADDomain).DistinguishedName
-$ObjectDN = "CN=dominic hughes\0ADEL:b659aafb-c59b-4dde-b96c-fd27ba618271,CN=Deleted Objects,$DomainDN"
-$Acl = Get-Acl "AD:\$ObjectDN"
-$Identity = New-Object System.Security.Principal.SecurityIdentifier((Get-ADUser svc_recovery).SID)
-$Rule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule(
-    $Identity,
-    [System.DirectoryServices.ActiveDirectoryRights]::GenericAll,
-    [System.Security.AccessControl.AccessControlType]::Allow
-)
-$Acl.AddAccessRule($Rule)
-Set-Acl "AD:\$ObjectDN" $Acl
+dsacls "DC=astera,DC=cg" /G "ASTERA\Recovery Operators:CA;Reanimate Tombstones"
+dsacls "CN=Deleted Objects,DC=astera,DC=cg" /takeownership
+dsacls "CN=Deleted Objects,DC=astera,DC=cg" /G "ASTERA\Recovery Operators:LCRP"
+dsacls "OU=DeletedUsers,OU=CORP,DC=astera,DC=cg" /I:T /G "ASTERA\Recovery Operators:WPCC"
+
+Get-ADObject -Filter 'isDeleted -eq $true' -IncludeDeletedObjects
